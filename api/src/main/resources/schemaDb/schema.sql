05817a3fe37bc6e72583cc85fde480f755836f7c
mhandrahani <mhandrahani91@gmail.com>
2023-12-26 21:25:15 +0100

    #2 update db schema last version

--- api/src/main/resources/schemaDb/schema.sql
+++ api/src/main/resources/schemaDb/schema.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
 --
--- Host: localhost    Database: tchat
+-- Host: localhost    Database: P13
 -- ------------------------------------------------------
 -- Server version	8.0.35-0ubuntu0.22.04.1
 
@@ -16,6 +16,143 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `agences`
+--
+
+DROP TABLE IF EXISTS `agences`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `agences` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  `zip_code` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `cars`
+--
+
+DROP TABLE IF EXISTS `cars`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cars` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `brand` varchar(45) NOT NULL,
+  `category` varchar(45) NOT NULL,
+  `gear_box` varchar(45) NOT NULL,
+  `localisation` int NOT NULL,
+  `agence_id` int NOT NULL,
+  `ecological_category` varchar(45) DEFAULT NULL,
+  `price_per_day` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_voitures_1_idx` (`agence_id`),
+  KEY `fk_cars_1_idx` (`localisation`),
+  CONSTRAINT `fk_cars_1` FOREIGN KEY (`localisation`) REFERENCES `drop-off-pick-up` (`id`),
+  CONSTRAINT `fk_voitures_1` FOREIGN KEY (`agence_id`) REFERENCES `agences` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `chat`
+--
+
+DROP TABLE IF EXISTS `chat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `chat` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int DEFAULT NULL,
+  `uuid` varchar(45) DEFAULT NULL,
+  `Receiver` varchar(45) DEFAULT NULL,
+  `content` varchar(250) DEFAULT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_chat_1_idx` (`user_id`),
+  CONSTRAINT `fk_chat_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `drop-off-pick-up`
+--
+
+DROP TABLE IF EXISTS `drop-off-pick-up`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `drop-off-pick-up` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `address` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `messages`
+--
+
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `messages` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) DEFAULT NULL,
+  `content` varchar(200) DEFAULT NULL,
+  `user_id` int DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_messages_1_idx` (`user_id`),
+  CONSTRAINT `fk_messages_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `payments`
+--
+
+DROP TABLE IF EXISTS `payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `payments` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `reservation_id` int NOT NULL,
+  `status` tinyint DEFAULT NULL,
+  `datetime` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_paiements_1_idx` (`reservation_id`),
+  CONSTRAINT `fk_paiements_1` FOREIGN KEY (`reservation_id`) REFERENCES `reservations` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `reservations`
+--
+
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservations` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `start_date` datetime(6) DEFAULT NULL,
+  `end_date` datetime(6) DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `car_id` int NOT NULL,
+  `drop-off_city` int NOT NULL,
+  `pick-up_city` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_reservations_1_idx` (`car_id`),
+  KEY `fk_reservations_2_idx` (`user_id`),
+  KEY `fk_reservations_3_idx` (`drop-off_city`),
+  KEY `fk_reservations_4_idx` (`pick-up_city`),
+  CONSTRAINT `fk_reservations_1` FOREIGN KEY (`car_id`) REFERENCES `cars` (`id`),
+  CONSTRAINT `fk_reservations_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `fk_reservations_3` FOREIGN KEY (`drop-off_city`) REFERENCES `drop-off-pick-up` (`id`),
+  CONSTRAINT `fk_reservations_4` FOREIGN KEY (`pick-up_city`) REFERENCES `drop-off-pick-up` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `roles`
 --
 
@@ -24,14 +161,13 @@ DROP TABLE IF EXISTS `roles`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `authority` varchar(45) NOT NULL,
-  `enabled` varchar(45) NOT NULL,
+  `authority` varchar(45) DEFAULT NULL,
+  `enabled` int NOT NULL,
   `user_id` int NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `id_UNIQUE` (`id`),
-  UNIQUE KEY `user_id_UNIQUE` (`user_id`),
-  CONSTRAINT `fk_roles_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  KEY `fk_roles_users_user_id` (`user_id`),
+  CONSTRAINT `fk_roles_users_user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -46,13 +182,14 @@ CREATE TABLE `users` (
   `email` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `date_of_birth` datetime(6) DEFAULT NULL,
+  `status` tinyint DEFAULT '0',
   `created_at` datetime(6) DEFAULT NULL,
   `updated_at` datetime(6) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `email_UNIQUE` (`email`),
-  UNIQUE KEY `name_UNIQUE` (`name`),
-  UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `name_UNIQUE` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -64,4 +201,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-21 23:51:10
+-- Dump completed on 2023-12-26 21:24:23
